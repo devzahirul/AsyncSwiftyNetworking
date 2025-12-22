c18c17be1e13fdbfa1716f6401e39147c39a6a43
zahirulAIIUB <jislam150@gmail.com>
2025-12-22 15:09:46 +0600

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AsyncSwiftyNetworking",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AsyncSwiftyNetworking",
+            targets: ["AsyncSwiftyNetworking"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AsyncSwiftyNetworking"
+        ),
+        .testTarget(
+            name: "AsyncSwiftyNetworkingTests",
+            dependencies: ["AsyncSwiftyNetworking"]
+        ),
+    ]
+)
